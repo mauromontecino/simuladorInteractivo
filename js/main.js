9cffe0d60cf9ba3b605e1ffce1579f378598ec6c
mauromontecino <mauromontecino2@gmail.com>
2022-07-05 16:15:40 -0300

    arrays y obj

--- js/main.js
+++ js/main.js
@@ -1,7 +1,52 @@
 //Pide información del producto
 
-let nombreDelProducto = prompt("Ingresar el producto");
-let precioDelProducto = parseInt(prompt("Ingresa su precio"));
+class Item {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre.toUpperCase();
+    this.precio = Number(precio);
+  }
+  productoNombPrec() {
+    console.log(this.id);
+    console.log(this.nombre);
+    console.log(this.precio);
+  }
+}
+
+const productos = [];
+
+let nombreDelProducto = [
+  productos.push(
+    new Item(
+      1,
+      prompt("Ingresa el plato principal"),
+      Number(prompt("Ingresa su precio"))
+    )
+  ),
+  productos.push(
+    new Item(
+      2,
+      prompt("Ingresa la guarnición"),
+      Number(prompt("Ingresa su precio"))
+    )
+  ),
+];
+
+for (const Item of productos) Item.productoNombPrec();
+
+//Muestra los productos
+
+console.log(productos);
+
+let precioDelProducto = parseInt(prompt("Ingresa su precio total"));
+
+//Lee el precio de los items
+
+for (const Item of productos) {
+  console.log(Item.precio);
+}
+
+//Ejecución
 
 do {
   opcion = Number(
@@ -9,7 +54,6 @@ do {
       "ingrese una opcion: \n\n 1- Tengo un cupon de descuento \n 2- No tengo cupon de descuento \n 3- Salir"
     )
   );
-  let cant;
 
   switch (opcion) {
     case 1:
@@ -26,6 +70,8 @@ do {
       break;
   }
 } while (opcion !== 3);
+
+//Si tiene descuento
 
 function tieneDescuento() {
   let porcentajeDeDescuento = parseInt(prompt("¿Cual es el % del descuento?"));
