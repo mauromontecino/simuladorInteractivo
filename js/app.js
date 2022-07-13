a8cbae7e56f35ff04d8d52b90dddea6863ebbb41
mauromontecino <mauromontecino2@gmail.com>
2022-07-13 15:25:33 -0300

    .

--- js/app.js
+++ js/app.js
@@ -11,36 +11,84 @@ document.getElementById(
   "menuPrincipal"
 ).innerHTML = `${nombre} . Esto es lo que te recomendamos`;
 
-//awdqwd
+//ProductosJs
 
 const productos = [
-  { nombre: "Hamburgueza", precio: 1100 },
-  { nombre: "Pizza", precio: 1300 },
-  { nombre: "Hamburgueza Completa", precio: 1800 },
-  { nombre: "Pizza Completa", precio: 2000 },
+  {
+    id: 1,
+    imagen: "./img/3.jpg",
+    nombre: "Hamburguesa",
+    categoria: "Plato principal",
+    precio: 1200,
+  },
+  {
+    id: 2,
+    imagen: "./img/12.jpg",
+    nombre: "Hamburguesa con papas fritas",
+    categoria: "Plato principal",
+    precio: 1800,
+  },
+  {
+    id: 3,
+    imagen: "./img/13.jpg",
+    nombre: "Hamburguesa con huevo",
+    categoria: "Plato principal",
+    precio: 1350,
+  },
+  {
+    id: 4,
+    imagen: "./img/14.jpg",
+    nombre: "Pin de pollo",
+    categoria: "Entrada",
+    precio: 500,
+  },
+  {
+    id: 5,
+    imagen: "./img/15.jpeg",
+    nombre: "Canape clasico",
+    categoria: "Entrada",
+    precio: 500,
+  },
+  {
+    id: 6,
+    imagen: "./img/16.jpeg",
+    nombre: "Carne al asador",
+    categoria: "Entrada",
+    precio: 800,
+  },
+  {
+    id: 7,
+    imagen: "./img/17.jpg",
+    nombre: "Ensalada de lechuga y tomate",
+    categoria: "Acompañamiento",
+    precio: 300,
+  },
 ];
-for (const producto of productos) {
-  let contenedor = document.createElement(`div`);
-  contenedor.className = "col-md-6 col-lg-4 col-xl-3";
-  //Definimos el innerHTML del elemento con una plantilla de texto
-  contenedor.innerHTML = `<div id="product-2" class="single-product">
-  <div class="part-1">
-  <img src="img/1.jpg" alt="">
-    <span class="discount">15% descuento</span>
-    <ul>
-      <li>
-        <a href="#"><i class="fa-solid fa-cart-plus"></i></a>
-      </li>
-      <li>
-        <a href="#"><i class="fas fa-heart"></i></a>
-      </li>
-    </ul>
+
+const carrito = [];
+
+function productosTienda() {
+  let tienda = document.getElementById("productosJS");
+
+  let divProduct = document.createElement("div");
+
+  divProduct.classList.add("single-product");
+
+  productos.forEach((product) => {
+    let productoHTML = `
+    <div class="card card-mod" style="width: 18rem;">
+    <div class="single-product">
+      <img class="card-img-top card-img-top-mod" src="${product.imagen}" alt="Card image cap" style="width: 262px; height: 262px">
+      <div class="card-body part-2">
+        <h3 class="card-title product-title">${product.nombre}</h3>
+        <h4 class="card-text">$${product.precio}</h4>
+        <br>
+        <a href="#" class="btn btn-primary">Agregar al carrito</a>
+      </div>
+    </div>
   </div>
-  <div class="part-2">
-    <h3 class="product-title">${producto.nombre}</h3>
-    <h4 class="product-old-price">$79.99</h4>
-    <h4 class="product-price">$${producto.precio}</h4>
-  </div>
-</div>`;
-  document.getElementById("productosJS").appendChild(contenedor);
+`;
+    tienda.innerHTML += productoHTML;
+  });
 }
+productosTienda();
